cf51be872922511131681126e6a916bd7ed754fe
M1571 <94639639+M1571@users.noreply.github.com>
2022-02-04 02:31:02 +0100

    name sur col

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-console.log('ciao')
+const nnname = prompt ('Inserisci il tuo nome');
+console.log (nnname);
+
+const surrrname = prompt ('Inserisci il tuo cognome');
+console.log (surrrname);
+
+const favcol = prompt ('Inserisci il tuo colore preferito');
+console.log (favcol);
